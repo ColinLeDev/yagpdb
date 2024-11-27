d852a956286037fcd8cc0eb935b662d986ecd113
Colin <colin.palazzetti.rubera@umontreal.ca>
2024-11-27 00:46:39 +0000

    LMAO/PTDR typo

--- common/templates/context.go
+++ common/templates/context.go
@@ -674,7 +674,7 @@ func baseContextFuncs(c *Context) {
 	c.addContextFunc("addReactions", c.tmplAddReactions)
 	c.addContextFunc("addResponseReactions", c.tmplAddResponseReactions)
 
-	c.addContextFunc("whoReated", c.tmplGetMessageReactionsUserList)
+	c.addContextFunc("whoReacted", c.tmplGetMessageReactionsUserList)
 	
 	c.addContextFunc("deleteAllMessageReactions", c.tmplDelAllMessageReactions)
 	c.addContextFunc("deleteMessageReaction", c.tmplDelMessageReaction)
