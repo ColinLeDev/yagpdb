37022ffd3012651615f58b90d141b17d2299bc16
Colin <colin.palazzetti.rubera@umontreal.ca>
2024-12-13 18:39:42 +0000

    https://github.com/botlabs-gg/yagpdb/pull/1795#discussion_r1884262446 Removed because unused https://github.com/botlabs-gg/yagpdb/pull/1795#discussion_r1884271314 Maybe change also the text ? https://github.com/botlabs-gg/yagpdb/pull/1795#discussion_r1884288576 Altered table

--- moderation/commands.go
+++ moderation/commands.go
@@ -1043,6 +1043,19 @@ var ModerationCommands = []*commands.YAGCommand{
 			).OneG(parsed.Context())
 			if err != nil {
 				return fmt.Sprintf("Could not find warning with ID `%d`", warningID), nil
+			warningID := parsed.Args[0].Int()
+
+			// Using that if we need to send to modlog
+			var warning *models.ModerationWarning
+			if config.DelwarnSendToModlog && config.ActionChannel != 0 {
+				warning, err = models.ModerationWarnings(
+					models.ModerationWarningWhere.ID.EQ(warningID),
+					// don't get warning from other servers, even if ID is correct
+					models.ModerationWarningWhere.GuildID.EQ(parsed.GuildData.GS.ID),
+				).OneG(parsed.Context())
+				if err != nil {
+					return fmt.Sprintf("Could not find warning with ID `%d`", warningID), nil
+				}
 			}
 
 			numDeleted, err := warning.DeleteG(parsed.Context())
